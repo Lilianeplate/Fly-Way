f264cdc68ba6f2a7a1d7c09370c0b0bef25be155
lilianeplate <liliane.plate@gmail.com>
2024-09-17 22:19:56 -0300

    destaques js

---
+++ dados.js
@@ -0,0 +1,98 @@
+let destaques = [
+    {
+        local: "Roma",
+        img: "src/img/novas_imagens/destaques/coliseo.jpg",
+        valor: 15000,
+        objetivo: "Estudo",
+        duracao: "3 semanas",
+        descricao: "Ao estudar na capital italiana, você mergulhará em uma rica história e cultura, aprimorará seu italiano, fará amigos do mundo inteiro e terá a oportunidade de explorar monumentos históricos e paisagens deslumbrantes. ️🇮🇹 Desvende os segredos do Coliseu, admire a beleza do Vaticano e saboreie a deliciosa culinária italiana. Prepare-se para uma experiência única que transformará sua vida!"
+    },
+    {
+        local: "Malta",
+        img: "src/img/novas_imagens/destaques/malta_noite.JPG",
+        valor: 10000,
+        objetivo: "Estudo",
+        duracao: "4 semanas",
+        descricao: "Descubra a ilha de Malta, um paraíso no Mediterrâneo! Com um clima agradável durante o ano todo, você poderá combinar seus estudos com atividades ao ar livre, como mergulho, snorkeling e passeios de barco. A ilha oferece um estilo de vida relaxado e uma cultura rica, com influências árabes, inglesas e francesas. Aprenda inglês em uma das escolas mais renomadas de Malta e explore as diversas opções de lazer que a ilha oferece."
+    },
+    {
+        local: "Austrália",
+        img: "src/img/novas_imagens/destaques/australia.jpg",
+        valor: 25000,
+        objetivo: "Estudo",
+        duracao: "6 semanas",
+        descricao: "Viva a experiência australiana! Estude em uma das cidades mais vibrantes do mundo e explore paisagens deslumbrantes, como a Grande Barreira de Coral e a Opera de Sydney. A Austrália oferece um alto padrão de vida, excelentes universidades e diversas oportunidades de trabalho. Aperfeiçoe seu inglês e faça amigos do mundo inteiro!"
+    },
+    {
+        local: "Canadá",
+        img: "src/img/novas_imagens/destaques/canada.jpg",
+        valor: 20000,
+        objetivo: "Estudo",
+        duracao: "3 semanas",
+        descricao: "Descubra a beleza do Canadá! Estude em uma das cidades mais multicultural do mundo e experimente a natureza exuberante do país. O Canadá oferece um sistema de ensino de alta qualidade, um custo de vida acessível e diversas oportunidades de trabalho. Aprenda inglês ou francês e explore as paisagens montanhosas, florestas e lagos do Canadá."
+    },
+    {
+        local: "Roma",
+        img: "src/img/novas_imagens/destaques/fontana.jpg",
+        valor: 15000,
+        objetivo: "Trabalho",
+        duracao: "3 semanas",
+        descricao: "Trabalhe em Roma e mergulhe em uma experiência única. Aprimore seu italiano, construa uma rede de contatos internacional e explore a Cidade Eterna. Com um mercado de trabalho diversificado e uma qualidade de vida excepcional, Roma te oferece a oportunidade de crescer profissionalmente e pessoalmente. Viva a experiência italiana!"
+    },
+    {
+        local: "Estados Unidos",
+        img: "src/img/novas_imagens/destaques/eua.jpg", 
+        valor: 5999,
+        objetivo: "Trabalho",
+        duracao: "2 semanas",
+        descricao: "Realize o sonho americano e trabalhe nos Estados Unidos! Desenvolva sua carreira em um dos países mais inovadores do mundo, explore diversas cidades e culturas, e aprimore seu inglês. Oportunidades em diversas áreas!"
+    },
+    {
+        local: "Malta",
+        img: "src/img/novas_imagens/malta_noite.JPG", 
+        valor: 5500,
+        objetivo: "Trabalho",
+        duracao: "2 semanas",
+        descricao: "Trabalhe em Malta, uma ilha paradisíaca com um alto padrão de vida. Aprenda inglês em um ambiente multicultural, explore as belas praias e cidades históricas, e tenha acesso fácil a outros países europeus."
+    },
+    {
+        local: "Canadá",
+        img: "src/img/novas_imagens/destaques/canada.jpg",
+        valor: 6200,
+        objetivo: "Trabalho",
+        duracao: "2 semanas",
+        descricao: "Trabalhe no Canadá e desfrute de um país com paisagens deslumbrantes, cidades modernas e um alto padrão de vida. Aprimore seu inglês e explore as diversas oportunidades de trabalho em um país multicultural."
+    },
+    {
+        local: "Roma",
+        img: "src/img/novas_imagens/destaques/parque_coliseo.jpg",
+        valor: 6000,
+        objetivo: "Férias",
+        duracao: "2 semanas",
+        descricao: "Descubra a magia de Roma em um intercâmbio de férias! Mergulhe na história milenar da Cidade Eterna, aprenda italiano com nativos e explore monumentos icônicos como o Coliseu e o Fórum Romano. Desfrute da deliciosa culinária italiana, faça novos amigos do mundo inteiro e vivencie a cultura local. Uma experiência única para aprimorar seu idioma, ampliar seus horizontes e criar memórias inesquecíveis."
+    },
+    {
+        local: "Malta",
+        img: "src/img/novas_imagens/malta_noite.JPG", 
+        valor: 3999,
+        objetivo: "Férias",
+        duracao: "2 semanas",
+        descricao: "Descubra a ilha de Malta, um paraíso no Mediterrâneo! Aprenda inglês em um ambiente multicultural, explore as belas praias, mergulhe em águas cristalinas e conheça a rica história da ilha. Desfrute de um verão inesquecível e faça amigos do mundo todo!"
+    },
+    {
+        local: "Canadá",
+        img: "src/img/novas_imagens/destaques/canada.jpg",
+        valor: 5999,
+        objetivo: "Férias",
+        duracao: "2 semanas",
+        descricao: "Viva a experiência de um inverno canadense! Aprenda inglês, esquie nas montanhas rochosas, explore cidades vibrantes como Toronto e Vancouver, e admire a beleza da aurora boreal. Uma aventura inesquecível em um dos países mais bonitos do mundo!"
+    },
+    {
+        local: "Nova Zelândia",
+        img: "src/img/novas_imagens/destaques/nova_zelandia.jpg",
+        valor: 7999,
+        objetivo: "Férias",
+        duracao: "2 semanas",
+        descricao: "Explore a natureza exuberante da Nova Zelândia! Faça trilhas, pratique esportes aquáticos, conheça a cultura maori e visite cenários de filmes famosos. Um paraíso para amantes da aventura e da natureza!"
+    }
+];
--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 // Slider
 
-let count = 1;
-document.getElementById("radio1").checked = true;
+let count = 1; // Inicializa o contador de slides
+document.getElementById("radio1").checked = true; // Seleciona o primeiro slide inicialmente
 
-setInterval( function (){
-    nextImage ();
-}, 9000)
+// Função para avançar para o próximo slide a cada 9 segundos
+setInterval(function() {
+    nextImage();
+}, 9000);
 
-
-function nextImage(){
-    count++;
-    if(count>4){
-    count = 1;
+function nextImage() {
+    count++; // Incrementa o contador de slides
+    if (count > 4) { // Verifica se o contador ultrapassou o número máximo de slides
+        count = 1; // Reinicia o contador para o primeiro slide
     }
 
-    document.getElementById("radio"+count).checked = true;
-
+    // Seleciona o rádio botão correspondente ao próximo slide
+    document.getElementById("radio" + count).checked = true;
 }
 
 
 
 
 
+//Destaques
 
+function mostrarDestaques(objetivo, secaoId) {
+    const secao = document.getElementById(secaoId);
+    secao.innerHTML = '';
+  
+    const destaquesFiltrados = destaques.filter(destaque => destaque.objetivo.toLowerCase() === objetivo.toLowerCase());
+    destaquesFiltrados.forEach(destaque => {
+      const novoDestaque = document.createElement('div');
+      novoDestaque.classList.add('destaque');
+      novoDestaque.innerHTML = `
+        <div class="pais_destaque">
+          <img src="${destaque.img}" alt="${destaque.local}">
+          <a href="#" class="texto">${destaque.local} a partir de R$${destaque.valor}</a>
+        </div>
+      `;
+      secao.appendChild(novoDestaque);
+    });
+  }
+  
+  // Chamar a função para cada seção
+  mostrarDestaques("Estudo", "estudos_destaques");
+  mostrarDestaques("Trabalho", "trabalho_destaques");
+  mostrarDestaques("Férias", "ferias_destaques");
 
